c2b77fd2516f7de1e1c7e761987ac5e03b4da48b
Viram Jain <viram.jain@kaleyra.com>
2021-08-13 11:13:41 +0530

    Final

--- Delete Key From Map/deleteKey.go
+++ Delete Key From Map/deleteKey.go
@@ -5,12 +5,12 @@ import (
 )
 
 //program to delete a key from map
-func main(){
+func main() {
 	myMap := make(map[int]int)
-	myMap[0]=0
-	myMap[1]=1
-	myMap[2]=2
-	fmt.Println("Map before deleting key ->",myMap)
-	delete(myMap,1)
-	fmt.Println("Map after deleting key ->",myMap)
+	myMap[0] = 0
+	myMap[1] = 1
+	myMap[2] = 2
+	fmt.Println("Map before deleting key ->", myMap)
+	delete(myMap, 1)
+	fmt.Println("Map after deleting key ->", myMap)
 }
--- Reverse Slice/reverseSlice.go
+++ Reverse Slice/reverseSlice.go
@@ -4,7 +4,7 @@ import "fmt"
 
 //program to reverse a slice order
 func main() {
-	nums := [...]int{1,2,3,4,5}
+	nums := [...]int{1, 2, 3, 4, 5}
 	fmt.Println("Before : ", nums)
 	reversed := []int{}
 	for i := range nums {
